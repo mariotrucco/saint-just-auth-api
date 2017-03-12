74adea18adc7d57b3e75c8aac6d358966a02ede5
mariotrucco <mario.trucco@gmail.com>
2017-03-12 17:54:08 +0100

    close #8

--- api/models/user.js
+++ api/models/user.js
@@ -1,16 +1,18 @@
 'use strict';
 
 var mongoose = require('mongoose');
+//use native Promise, see http://mongoosejs.com/docs/promises.html
+mongoose.Promise = global.Promise;
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-	username	: String,
-	email		: String,
-	verified	: Boolean,
-	country		: String,
-	locale		: String,
-	timezone	: String
+	username	: { type : String,	unique : true,	required : true },
+	email		: { type : String,	unique : true,	required : true },
+	verified	: { type : Boolean, unique : false, required : true },
+	country		: { type : String,	unique : false, required : true },
+	locale		: { type : String,	unique : false, required : true },
+	timezone	: { type : String,	unique : false, required : true }
 });
 
 User.plugin(passportLocalMongoose);
